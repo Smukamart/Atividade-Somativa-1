41f02040a9f32a9eceb434ca382b69755dd70185
Samuel Martins <samukamartins007@gmail.com>
2026-04-17 21:18:19 -0300

    Create Routes Finished

---
+++ src/paginas/Contato/index.js
@@ -0,0 +1,10 @@
+function Contact() {
+    return (
+        <div>
+            <h1>Contato</h1>
+            <p>Entre em contato conosco através do email: contato@exemplo.com</p>
+        </div>
+    );
+}
+
+export default Contact;
---
+++ src/paginas/Sobre/index.js
@@ -0,0 +1,10 @@
+function About() {
+    return (
+        <div>
+            <h1>Sobre</h1>
+            <p>Este é um projeto de exemplo para demonstrar a criação de uma página "Sobre" em React.</p>
+        </div>
+    );
+}
+
+export default About;
